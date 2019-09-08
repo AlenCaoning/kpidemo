65081ac14bd49083c404afbbbbf42d2f6f568afc
Cao <Alen.Cao@connext.com.cn>
2019-09-08 16:34:42 +0800

    Initial commit

---
+++ src/main/java/com/connext/alen/KpidemoApplication.java
@@ -0,0 +1,13 @@
+package com.connext.alen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpidemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpidemoApplication.class, args);
+	}
+
+}
